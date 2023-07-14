107afb48a079f7146cb1134bba73df0afdd0ee80
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-07-14 12:44:19 -0500

    Agregados los elementos del combo box

--- src/Fomulario.java
+++ src/Fomulario.java
@@ -22,9 +22,9 @@ public class Fomulario extends JFrame implements ItemListener {
 
     /*Combo Box Variables -------*/
     private JComboBox <String> signoBox;
-    private JComboBox dayBox;
-    private JComboBox mesBox;
-    private JComboBox yearBox;
+    private JComboBox<String> dayBox;
+    private JComboBox<String> mesBox;
+    private JComboBox<String> yearBox;
     private JLabel sateLabel;
     /*---------------------------*/
     private JLabel colorLabel;
@@ -43,37 +43,67 @@ public class Fomulario extends JFrame implements ItemListener {
     ArrayList<Estudiante> people = new ArrayList<Estudiante>();
 
     /*Variables y elementos añadidos para combo boxes*/
-
+    //String [] signos = {"Aries", "Leo", "Cáncer", "Virgo", "Escorpio"};
     /*-----------------------------------------------*/
 
-
-    /*Acciones del panel*/
     public Fomulario() {
-        /*Creamos acciones de los combo box*/
-        signoBox = new JComboBox();
-        signoBox.addItem("Acuario");
+        /*Signo Combo Box*/
         signoBox.addItem("Aries");
-        signoBox.addItem("Cáncer");
-        signoBox.addItem("Capricornio");
+        signoBox.addItem("Leo");
         signoBox.addItem("Escorpio");
-        signoBox.setSelectedItem("Acuario");
+        signoBox.addItem("Cáncer");
+        signoBox.addItem("Libra");
+        signoBox.addItemListener(this);
 
+        /*----FechaBox----*/
+        dayBox.addItem("1");
+        dayBox.addItem("2");
+        dayBox.addItem("3");
+        dayBox.addItem("4");
+        dayBox.addItem("5");
+        dayBox.addItem("6");
+        dayBox.addItem("7");
+        dayBox.addItem("8");
+        dayBox.addItem("9");
+        dayBox.addItem("10");
+        dayBox.addItemListener(this);
+
+        mesBox.addItem("Enero");
+        mesBox.addItem("Febrero");
+        mesBox.addItem("Marzo");
+        mesBox.addItem("Abril");
+        mesBox.addItem("Mayo");
+        mesBox.addItem("Junio");
+        mesBox.addItem("Julio");
+        mesBox.addItem("Agosto");
+        mesBox.addItem("Septiembre");
+        mesBox.addItem("Octubre");
+        mesBox.addItem("Noviembre");
+        mesBox.addItem("Diciembre");
+        mesBox.addItemListener(this);
+
+        yearBox.addItem("1999");
+        yearBox.addItem("2000");
+        yearBox.addItem("2001");
+        yearBox.addItem("2002");
+        yearBox.addItem("2003");
+        yearBox.addItemListener(this);
+        /*-----------------------------------------*/
 
         guardarDatosButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            /*Instanciamos el objeto*/
+                /*Instanciamos el objeto*/
                 Estudiante person = new Estudiante(nameInput.getText(), lastInput.getText(), ccInput.getText(), codeInput.getText());
 
-                for(int i=0; i<=items; i=i+1){
+                for (int i = 0; i <= items; i = i + 1) {
                     people.add(person);
                 }
 
-                try(FileOutputStream fileOutputStream = new FileOutputStream(filepath);
-                    ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream))
-                {
-                    for (int i=0; i<people.size(); i++){
-                        if((people.get(i++).equals(people.get(i-1)))) break;
+                try (FileOutputStream fileOutputStream = new FileOutputStream(filepath);
+                     ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream)) {
+                    for (int i = 0; i < people.size(); i++) {
+                        if ((people.get(i++).equals(people.get(i - 1)))) break;
                         else {
                             outputStream.writeObject(people.get(i).getNombres());
                             outputStream.writeObject(people.get(i).getApellidos());
@@ -81,7 +111,7 @@ public class Fomulario extends JFrame implements ItemListener {
                             outputStream.writeObject(people.get(i).getCodigo());
                         }
                     }
-                }catch (IOException exception) {
+                } catch (IOException exception) {
                     throw new RuntimeException(new Exception(exception));
                 }
                 nameInput.setText("");
@@ -94,24 +124,17 @@ public class Fomulario extends JFrame implements ItemListener {
         cargarDatosButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try (FileInputStream fileInputStream=new FileInputStream(filepath);
-                     ObjectInputStream objectInputStream=new ObjectInputStream(fileInputStream))
-                {
-                    for (Estudiante person:people){
+                try (FileInputStream fileInputStream = new FileInputStream(filepath);
+                     ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+                    for (Estudiante person : people) {
                         nameInput.setText(person.getNombres());
                         lastInput.setText(person.getApellidos());
                         ccInput.setText(person.getCedula());
                         codeInput.setText(person.getCodigo());
                     }
-                }catch (IOException exception){
+                } catch (IOException exception) {
                     throw new RuntimeException(exception);
                 }
-            }
-        });
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
             }
         });
     }
@@ -128,6 +151,9 @@ public class Fomulario extends JFrame implements ItemListener {
 
     @Override
     public void itemStateChanged(ItemEvent e) {
-
+        if(e.getSource() == signoBox){
+            String seleccion = (String) signoBox.getSelectedItem();
+            setTitle(seleccion);
+        }
     }
 }
